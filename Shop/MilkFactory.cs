ad81089c9538124ee2030e14aa0bbce8abd70cae
Slezak Attila <attila.slezak@gmail.com>
2016-09-24 19:05:33 +0100

    Create MilkFactory class - Part III - 3

---
+++ Shop/MilkFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public abstract class MilkFactory
+    {
+        public static Milk NewLongLifeMilk(long barcode, int cubicCapacity, String producer, DateTime bestBefore, double fatContent)
+        {
+            return new LongLifeMilk(barcode, cubicCapacity, producer, bestBefore, fatContent);
+        }
+
+        public static Milk NewSemiLongLifeMilk(long barcode, int cubicCapacity, String producer, DateTime bestBefore, double fatContent)
+        {
+            return new SemiLongLifeMilk(barcode, cubicCapacity, producer, bestBefore, fatContent);
+        }
+    }
+}
